897e298c5747dcb3bc5967d0967a5fd414ac6e1a
night1221 <140440869+night1221@users.noreply.github.com>
2024-08-05 22:06:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ window.onload = function(){
         name.innerHTML = verif;
     }
 }
+
+
+
+let count = 0
+
+function qwe(){
+    document.getElementById("rd").innerHTML = "Cancel"
+    document.getElementById("rd").style.background = "red"
+    document.getElementById("rd").onclick = qwee;
+    count += 1;
+    function qwee(){
+        document.getElementById("rd").innerHTML = "Ready"
+        document.getElementById("rd").style.background = "green"
+        count += 1;
+        if(Number.isInteger(count/2) ){
+            
+            document.getElementById("rd").onclick = qwe;
+        }else{
+            document.getElementById("rd").onclick = qwe;
+        }
+    }
+}
+
+qwe()
